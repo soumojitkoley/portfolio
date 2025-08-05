ccecc64a68b7f8883ad7149ac799644fc68c27f0
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2025-08-05 22:32:32 +0530

    fixed locomotive css glitch for smooth scrolling and added About component which will have the about part and added css as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
+import About from './components/About'
+import LocomotiveScroll from 'locomotive-scroll';
 import './App.css'
 
 function App() {
-
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true
+    });
+  
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
   return (
-    <div className='content'>
-      <div className='container'>
+    <div className='app-container' data-scroll-container>
+      <div className="nav-part">
         <Navbar />
-        <h1>My name is Soumojit Koley</h1>
       </div>
+      <div className="about-part">
+        <About/>
+      </div>
+      <div className="about-part1">
+
+      </div>
+      <div className="about-part1">
+
+      </div>
+      <div className="about-part1">
+
+      </div>
+      <div className="about-part1">
+
+      </div>
+      <div className="about-part1">
+
+      </div>
+      
     </div>
   )
 }
