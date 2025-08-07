51f1c0b818fbe411366a0ef46ec58708a331b6c4
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2025-08-07 23:58:45 +0530

    Made Navbar and About part responsive and fixed few bugs and deleted some unused code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoLocationOutline, IoMailOutline  } from "react-icons/io5";
 import { IoIosMail, IoMdDownload } from "react-icons/io";
 import { FaSquareXTwitter, FaLocationDot } from "react-icons/fa6";
 
@@ -16,10 +17,14 @@ const About = () => {
           <h2>Software Engineer</h2>
         </div>
         <div className="about-part1-email-location">
-          <IoIosMail />
-          <p>koleysoumojit6@gmail.com</p>
-          <FaLocationDot />
-          <p>Kolkata</p>
+          <div className='about-part1-email'>
+            <IoMailOutline size={19}/>
+            <a href="mailto:koleysoumojit6@gmail.com">koleysoumojit6@gmail.com</a>
+          </div>
+          <div className='about-part1-location'>
+            <IoLocationOutline size={19}/>  
+            <p>Kolkata, India</p>
+          </div>
         </div>
         <div className="about-part1-bio">
           <p>A goal-oriented software developer with experience in building web applications using modern technologies like React, Next.js, and more. Seeking to leverage my technical skills to deliver exceptional user experiences.</p>
@@ -29,15 +34,15 @@ const About = () => {
             <IoMdDownload size={20}/>
             <p>Resume</p>
           </div>
-          <div className="about-part1-link-box"><FaGithub size={20}/></div>
-          <div className="about-part1-link-box"><IoIosMail size={25}/></div>
-          <div className="about-part1-link-box"><FaSquareXTwitter size={20} /></div>
-          <div className="about-part1-link-box"><FaLinkedin size={20}/></div>
+          <a href="https://github.com/soumojitkoley" target='_blank'><div className="about-part1-link-box"><FaGithub size={20}/></div></a>
+          <a href="mailto:koleysoumojit6@gmail.com"><div className="about-part1-link-box"><IoIosMail size={25}/></div></a>
+          <a href="https://x.com/soumo_dev" target='_blank'><div className="about-part1-link-box"><FaSquareXTwitter size={20} /></div></a>
+          <a href="https://www.linkedin.com/in/soumojitkoley/" target='_blank'><div className="about-part1-link-box"><FaLinkedin size={20}/></div></a>
         </div>
       </div>
       <div className="about-part2">
         <div className="about-part2-techbox-row-1">
-          <div className="tech-box"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" /></div>
+          <div className="tech-box"><img src="https://assets.chanhdai.com/images/tech-stack-icons/js.svg" /></div>
           <div className="tech-box"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" /></div>
         </div>
         <div className="about-part2-techbox-row-2">
@@ -49,4 +54,4 @@ const About = () => {
   )
 }
 
-export default About
+export default About  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,21 @@
 import React from 'react';
-import './Navbar.css';
-import { NavLink, useLocation } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import LocomotiveScroll from 'locomotive-scroll';
-const locomotiveScroll = new LocomotiveScroll();
+import './Navbar.css';
 
 const Navbar = () => {
-  const location = useLocation()
   const isMobile = useMediaQuery({ maxWidth: 768 })
   const [isMenuOpen, setMenuOpen] = useState(false)
-  const [path, setPath] = useState(false)
 
   const handleMenuToggle = () => {
     setMenuOpen(!isMenuOpen)
   };
 
-  useEffect(() => {
-    if (location.pathname == '/doctors' || location.pathname == '/hospitals' || location.pathname == '/blood-bank') {
-      setPath(true)
-    }
-  }, [location.pathname])
-
-  function scrollHandler(scrollTarget) {
-    if (scrollTarget == '') {
-      locomotiveScroll.scrollTo(0, 0)
-    }
-    else if (scrollTarget == '#abt' && isMobile == false) {
-      locomotiveScroll.scrollTo(770)
-    }
-    else if (scrollTarget == '#svc' && isMobile == false) {
-      locomotiveScroll.scrollTo(1320)
-    }
-    else if (scrollTarget == '#abt' && isMobile == true) {
-      locomotiveScroll.scrollTo(1570)
-    }
-    else if (scrollTarget == '#svc' && isMobile == true) {
-      locomotiveScroll.scrollTo(2670)
-    }
-    else {
-      locomotiveScroll.scrollTo(scrollTarget)
-    }
-  }
 
   return (
     <div className="navbar">
-      <h1 onClick={() => { scrollHandler('') }} className='navbar-h1'><span className='blue'>Soumojit</span><span className='green'>Koley</span></h1>
+      <h2 onClick={() => { scrollHandler('') }} className='navbar-h1'>Soumojit.</h2>
       {isMobile ? (
         <div className='hello'>
           <label class="burger" for="burger">
@@ -64,51 +33,35 @@ const Navbar = () => {
                 transition={{ duration: 0.3 }}
               >
                 <div className="nav-menu">
+                <h2 onClick={() => { scrollHandler('') }} className='navbar-h1-mobile'>Soumojit.</h2>
                   <ul type="none" className="list">
                     <li>
-                      <NavLink onClick={() => { scrollHandler(''), handleMenuToggle() }} to="/" className="link">
-                        Home
-                      </NavLink>
-                    </li>
-                    {
-                      !path ? (
-                        <li>
-                          <a onClick={() => { scrollHandler('#abt'), handleMenuToggle() }} className="link">
-                            About
-                          </a>
-                        </li>
-                      )
-                        :
-                        (<></>)
-                    }
-                    {
-                      !path ? (
-                        <li>
-                          <a onClick={() => { scrollHandler('#svc'), handleMenuToggle() }} className="link">
-                            Services
-                          </a>
-                        </li>
-                      )
-                        :
-                        (<></>)
-                    }
-                    <li>
-                      <NavLink onClick={handleMenuToggle} to="/doctors" className="link">
-                        Doctors
-                      </NavLink>
+                      <a onClick={() => scrollHandler('#abt')} className="link">
+                        About
+                      </a>
                     </li>
                     <li>
-                      <NavLink onClick={handleMenuToggle} to="/hospitals" className="link">
-                        Hospitals
-                      </NavLink>
+                      <a onClick={() => scrollHandler('#abt')} className="link">
+                        Experience
+                      </a>
                     </li>
                     <li>
-                      <NavLink onClick={handleMenuToggle} to="/blood-bank" className="link">
-                        Blood Bank
-                      </NavLink>
+                      <a onClick={handleMenuToggle} className="link">
+                        Education
+                      </a>
                     </li>
                     <li>
-                      <a onClick={() => { scrollHandler('#ft'), handleMenuToggle() }} className="link">
+                      <a onClick={() => scrollHandler('#svc')} className="link">
+                        Skills
+                      </a>
+                    </li>
+                    <li>
+                      <a onClick={() => scrollHandler('#svc')} className="link">
+                        Projects
+                      </a>
+                    </li>
+                    <li>
+                      <a onClick={() => scrollHandler('#ft')} className="link">
                         Contact
                       </a>
                     </li>
@@ -122,46 +75,29 @@ const Navbar = () => {
         <div className="nav-menu">
           <ul type="none" className="list">
             <li>
-              <NavLink onClick={() => scrollHandler('')} to="/" className="link">
-                Home
-              </NavLink>
-            </li>
-            {
-              !path ? (
-                <li>
-                  <a onClick={() => scrollHandler('#abt')} className="link">
-                    About
-                  </a>
-                </li>
-              )
-                :
-                (<></>)
-            }
-            {
-              !path ? (
-                <li>
-                  <a onClick={() => scrollHandler('#svc')} className="link">
-                    Services
-                  </a>
-                </li>
-              )
-                :
-                (<></>)
-            }
-            <li>
-              <NavLink to="/doctors" className="link">
-                Doctors
-              </NavLink>
+              <a onClick={() => scrollHandler('#abt')} className="link">
+                About
+              </a>
             </li>
             <li>
-              <NavLink to="/hospitals" className="link">
-                Hospitals
-              </NavLink>
+              <a onClick={() => scrollHandler('#abt')} className="link">
+                Experience
+              </a>
             </li>
             <li>
-              <NavLink to="/blood-bank" className="link">
-                Blood Bank
-              </NavLink>
+              <a onClick={() => scrollHandler('#svc')} className="link">
+                Education
+              </a>
+            </li>
+            <li>
+              <a onClick={() => scrollHandler('#svc')} className="link">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a onClick={() => scrollHandler('#svc')} className="link">
+                Projects
+              </a>
             </li>
             <li>
               <a onClick={() => scrollHandler('#ft')} className="link">
