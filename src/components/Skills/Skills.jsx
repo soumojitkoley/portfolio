9814cda6b63da6065b08657ceab398222343eef4
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2025-08-15 22:55:37 +0530

    Almost done with Skills part and css, just some minor changes are needed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import skillsData from '../../data/skills.js'
+import './Skills.css'
 const Skills = () => {
   return (
     <div className='skill-container' id='skills'>
@@ -8,7 +9,30 @@ const Skills = () => {
           <h1>Skills</h1>
         </div>
         <div className="skill-list">
-          
+          {
+            skillsData.map((eachData, idx) => (
+              <div className="skill-category">
+                <div className='skill-catogory-name'>
+                  <h1>{eachData.name}</h1>
+                </div>
+                <div className='skill-category-item'>
+                  {
+                    eachData.icons.map((eachLogo, idx) => (
+                      <div className='skill-tech-icon-name' key={idx}>
+                        <div className="skill-tech-icon">
+                          <img src={eachLogo.src} alt="" />
+                        </div>
+
+                        <div className="skill-tech-name">
+                          <p>{eachLogo.name}</p>
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
