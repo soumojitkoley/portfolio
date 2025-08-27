4afe73b61f8477d5fad582e1e219c0bffc60bcb1
SOUMOJIT KOLEY <koleysoumojit6@gmail.com>
2025-08-27 23:31:39 +0530

    Cleaned all the unused jsx and css codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Navbar from './components/Navbar/Navbar.jsx'
 import About from './components/About/About.jsx'
 import Experience from './components/Experience/Experience.jsx';
